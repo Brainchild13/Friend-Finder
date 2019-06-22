ecfdba9754aa885d713daea459f903bfbd29b5b8
David Schwartz <davidschwartz23@gmail.com>
2019-06-21 20:26:08 -0700

    made corrections in friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,21 @@
 var userArray = [
-
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        3,
-        1,
-        2,
-        4,
-        4,
-        1,
-        2
-        5,
-        1
-        1
-      ]
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 1, 2, 4, 4, 1, 2, 5, 1, 1]
   },
   {
-    "name":"Jacob",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        1,
-        5
-        2,
-        4,
-        1,
-        4,
-        2,
-        3,
-        4,
-        2
-      ]
+    name: "Jacob",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 5, 2, 4, 1, 4, 2, 3, 4, 2]
   },
   {
-    "name":"Daniel",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        3,
-        1,
-        3,
-        3,
-        5,
-        3,
-        2,
-        3,
-        3,
-        1
-      ]
+    name: "Daniel",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 1, 3, 3, 5, 3, 2, 3, 3, 1]
   }
 ];
 
